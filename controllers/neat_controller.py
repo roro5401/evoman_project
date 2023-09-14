288db9b9c26d24e8c56a4ec7ef3f2baa48d4431a
robbert-rog <robbertrog@gmail.com>
2023-09-14 13:50:02 +0200

    implemented a controller that can be initialized from NEAT genomes.

--- controllers/neat_controller.py
+++ controllers/neat_controller.py
@@ -6,6 +6,32 @@ class neat_controller(Controller):
 		self.neural_network = neat.nn.FeedForwardNetwork.create(genome=genome, config=config)
 
 	def control(self, params, cont=None):
-		output_1 = self.neural_network.activate(inputs=params)
+		output = self.neural_network.activate(inputs=params)
+		if output[0] > 0.5:
+			left = 1
+		else:
+			left = 0
+
+		if output[1] > 0.5:
+			right = 1
+		else:
+			right = 0
+
+		if output[2] > 0.5:
+			jump = 1
+		else:
+			jump = 0
+
+		if output[3] > 0.5:
+			shoot = 1
+		else:
+			shoot = 0
+
+		if output[4] > 0.5:
+			release = 1
+		else:
+			release = 0
+
+		return [left, right, jump, shoot, release]
 
 
