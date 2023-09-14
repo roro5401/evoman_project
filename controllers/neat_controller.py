45ed7dda9bb6f2801031a694756735a5ae4195c7
robbert-rog <robbertrog@gmail.com>
2023-09-14 13:55:43 +0200

    made much more compact controller

--- controllers/neat_controller.py
+++ controllers/neat_controller.py
@@ -7,31 +7,6 @@ class neat_controller(Controller):
 
 	def control(self, params, cont=None):
 		output = self.neural_network.activate(inputs=params)
-		if output[0] > 0.5:
-			left = 1
-		else:
-			left = 0
-
-		if output[1] > 0.5:
-			right = 1
-		else:
-			right = 0
-
-		if output[2] > 0.5:
-			jump = 1
-		else:
-			jump = 0
-
-		if output[3] > 0.5:
-			shoot = 1
-		else:
-			shoot = 0
-
-		if output[4] > 0.5:
-			release = 1
-		else:
-			release = 0
-
-		return [left, right, jump, shoot, release]
+		return [round(action) for action in output]
 
 
