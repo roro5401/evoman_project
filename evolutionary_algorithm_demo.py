6ef18e3e2c0336e4cee77f71056d1c24c983a66c
Wschuurman <wiegerschuurman@gmail.com>
2023-10-09 12:39:57 +0200

    mutation draft

--- evolutionary_algorithm_demo.py
+++ evolutionary_algorithm_demo.py
@@ -1,9 +1,23 @@
 from controllers.genome_demo_controller import GenomeDemoController
 from controllers.demo_controller import player_controller
+import math
+import numpy as np
 
 """"Function should return new list of sigma_stepsizes"""
 def update_sigma_stepsize(sigma_stepsizes: list) -> list:
-    pass
+    boundary = 0.01
+    new_sigma_stepsizes = []
+    n = len(sigma_stepsizes)
+    tau = 1/math.sqrt(2*n)
+    tau_accent = 1/math.sqrt(2*math.sqrt(n))
+    N_0_1 = np.random.normal(0,1,1)
+    N_0_1_list = np.random.normal(0,1,n)
+    for i in range(0,n):
+        new_sigma = sigma_stepsizes[i]*math.exp(tau_accent*N_0_1 + tau*N_0_1_list[i])
+        if new_sigma < boundary:
+            new_sigma = boundary
+        new_sigma_stepsizes.append(new_sigma)
+    return new_sigma_stepsizes
 
 
 """Function should return dict like below"""
@@ -17,6 +31,9 @@ def uncorrelated_mutation(genome: GenomeDemoController) -> dict:
     # do mutation on weights and biases
 
     new_weights_and_biases = []
+    for i in range(0, len(weights_and_bias)):
+        new_weight = weights_and_bias[i] + new_sigma_stepsizes[i]*np.random.normal(0,1,1)
+        new_weights_and_biases.append(new_weight)
 
     return {"weights_and_biases": new_weights_and_biases, "sigma_stepsizes": new_sigma_stepsizes}
 
